8a8630cd894818ace8300330d01b35cf3c2dc9b7
ineedhealing <gregiosdev@gmail.com>
2018-01-23 15:30:23 +0200

    Initial Commit

---
+++ WebPageScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebPageScanner
+//
+//  Created by Greg on 1/23/18.
+//  Copyright © 2018 Egor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebPageScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebPageScanner
+//
+//  Created by Greg on 1/23/18.
+//  Copyright © 2018 Egor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
